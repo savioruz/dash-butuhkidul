cc21e38df655fffe2731ed5e0f1b0ceef2b4f80e
savioruz <kheril.dev@gmail.com>
2025-08-25 19:46:26 +0700

    update metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
-	name: 'sv',
+	name: 'dashboard butuhkidul',
 	logo: '/favicon.svg',
-	description: '@TODO - Replace with your site description',
-	keywords: '@TODO - Replace with your site keywords',
-	url: new URL('https://example.com'), // @TODO - Replace with your site URL
-	ogImage: new URL('https://example.com/og.png'), // @TODO - Replace with your Open Graph image URL
+	description: 'A dashboard for managing butuhkidul site',
+	keywords: '',
+	url: new URL('https://dash.butuhkidul.my.id'),
+	ogImage: new URL('https://dash.butuhkidul.my.id/og.png'),
 	links: {
 		twitter: new URL('https://twitter.com/example'), // @TODO - Replace with your Twitter URL
 		github: new URL('https://github.com/example') // @TODO - Replace with your GitHub URL
